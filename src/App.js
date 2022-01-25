2177b75c197c2b30fccd31733b7590a9207c89a3
Rene Copaga <rene.copaga@gmail.com>
2022-01-25 15:58:49 -0400

    Acceptance Tests for Book List -A List (of Books)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
       </Typography>
+      <div data-test='book-list'>
+        <div className='book-item'>
+        </div>
+        <div className='book-item'>
+        </div>
+      </div>
     </div>
   );
 }
