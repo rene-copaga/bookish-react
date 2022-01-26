3f947d34bccb7d2f84b2146fc917c729648142d5
Rene Copaga <rene.copaga@gmail.com>
2022-01-25 22:09:45 -0400

    Adding a Loading Indicator -Refactor First --Adding More States

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ import BookList from "./BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await axios.get('http://localhost:8080/books');
       setBooks(res.data);
+      setError(false);
+      setLoading(true);
+
+      try {
+        const res = await axios.get('http://localhost:8080/books');
+        setBooks(res.data);
+      } catch (e) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchBooks();
