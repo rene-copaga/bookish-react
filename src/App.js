8fe9327f65a749a882178e62d368678b02bad2e2
Rene Copaga <rene.copaga@gmail.com>
2022-01-25 22:56:18 -0400

    --Refactor: Extract Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Typography } from '@material-ui/core';
-import axios from 'axios';
 
-import BookList from "./BookList";
+import BookListContainer from "./BookListContainer";
 
 function App() {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const res = await axios.get('http://localhost:8080/books');
-      setBooks(res.data);
-      setError(false);
-      setLoading(true);
-
-      try {
-        const res = await axios.get('http://localhost:8080/books');
-        setBooks(res.data);
-      } catch (e) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchBooks();
-  }, []);
-
   return (
     <div>
       <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
       </Typography>
-      <BookList books={books} />
+      <BookListContainer />
     </div>
   );
 }
