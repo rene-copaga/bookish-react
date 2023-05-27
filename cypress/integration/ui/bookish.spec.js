02917971365116d3cc832c5c13f40810eabbf068
Rene Copaga <rene.copaga@gmail.com>
2023-05-27 19:14:01 -0400

    -Verify Book Title on Detail Page

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -47,5 +47,6 @@ describe('Bookish application', function() {
     cy.visit('http://localhost:3000/');
     cy.get('div.book-item').contains('View Details').eq(0).click();
     cy.url().should('include', '/books/1');
+    cy.get('h2.book-title').contains('Refactoring');
   });
 })
