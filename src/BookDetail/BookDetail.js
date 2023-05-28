4e76f4543feb947c33cf8acc8623af26a8097738
Rene Copaga <rene.copaga@gmail.com>
2023-05-28 12:28:16 -0400

    Handling default value -A Failing Test with undefined

--- src/BookDetail/BookDetail.js
+++ src/BookDetail/BookDetail.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+const getDescriptionFor = (book) => {
+    return book.description ? book.description : book.name;
+}
+
 const BookDetail = ({book}) => {
     return (
         <div className='detail'>
             <h2 className='book-title'>{book.name}</h2>
-            <p className='book-description'>{book.description}</p>
+            <p className='book-description'>{getDescriptionFor(book)}</p>
         </div>
     )
 }
